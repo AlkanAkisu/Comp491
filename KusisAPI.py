8ca448cb6307625c2b1236b27823817fc8444e40
Alkan AKISU <aakisu19@ku.edu.tr>
2022-03-28 19:47:04 +0300

    4 new use cases added

--- KusisAPI.py
+++ KusisAPI.py
@@ -47,13 +47,37 @@ class KusisAPI:
             code, name, semester, grade, credits, *other = data
             record = AcademicRecord(code, name, semester, grade, credits)
             records.append(record)
-
+            
         return records
         # json_string = json.dumps([record.__dict__ for record in records])
         # print(json_string)
 
         # //*[@id="trCRSE_HIST$0_row6"]
+    def getGPA(self):
 
+        url = "https://kusis.ku.edu.tr/psc/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ACADEMIC_RECORDS.HC_SSS_MY_CRSEHIST_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder&PortalActualURL=https%3a%2f%2fkusis.ku.edu.tr%2fpsc%2fps%2fEMPLOYEE%2fSA%2fc%2fSA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL&PortalContentURL=https%3a%2f%2fkusis.ku.edu.tr%2fpsc%2fps%2fEMPLOYEE%2fSA%2fc%2fSA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL&PortalContentProvider=SA&PortalCRefLabel=My%20Course%20History&PortalRegistryName=EMPLOYEE&PortalServletURI=https%3a%2f%2fkusis.ku.edu.tr%2fpsp%2fps%2f&PortalURI=https%3a%2f%2fkusis.ku.edu.tr%2fpsc%2fps%2f&PortalHostNode=SA&NoCrumbs=yes&PortalKeyStruct=yes"
+
+        payload = {}
+        headers = {
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Sec-GPC': '1',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Dest': 'iframe',
+            'Referer': 'https://kusis.ku.edu.tr/psp/ps/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.SSS_MY_CRSEHIST.GBL?FolderPath=PORTAL_ROOT_OBJECT.CO_EMPLOYEE_SELF_SERVICE.HCCC_ACADEMIC_RECORDS.HC_SSS_MY_CRSEHIST_GBL&IsFolder=false&IgnoreParamTempl=FolderPath%2cIsFolder',
+            'Accept-Language': 'tr',
+            'Cookie': self.cookie
+        }
+        response = requests.request("GET", url, headers=headers, data=payload)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        table = soup.select('#win0divKU_AH_EDHIST_VW\\$0')[0]
+        gpa = table.findChildren('tr')[-1].findChildren('td')[-1].text.strip()
+        return {'gpa': gpa}
+        
 
 class AcademicRecord:
     def __init__(self, code, name, semester, grade, credits) -> None:
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from replit import web
 from BlackBoardAPI import BlackBoardAPI
 from KusisAPI import KusisAPI
 import json
+from urllib.request import urlopen
 
 
 # Create a flask app
@@ -11,6 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   return 'hello'
+  
 # Index page
 @app.route('/allCourses')
 def getAllCourses():
@@ -21,6 +23,26 @@ def getAllCourses():
   r = make_response( json_string )
   r.mimetype = 'application/json'
   return r
+
+@app.route('/getCourseID')
+def getCourseID():
+  courseCode = request.args.get('courseName')
+  cookie = request.headers.get('Cookie')
+  bb_API = BlackBoardAPI(cookie)
+  courses = bb_API.getAllCourses()
+  foundCourse = None
+  for course in courses:
+    if course.courseCode == courseCode:
+      if course.courseSection.isdigit():
+        foundCourse = course
+        break
+  if foundCourse == None:
+    return {
+      "courseId": -1
+    }
+  return {
+    "courseId": foundCourse.courseID
+  } 
   
 @app.route('/mygrades/<course_id>')
 def getGrade(course_id):
@@ -55,6 +77,16 @@ def getLetterGrades():
   json_string = json.dumps([record.__dict__ for record in records])
   r = make_response( json_string )
   r.mimetype = 'application/json'
+  return r
+  
+@app.route('/gpa')
+def getGPA():
+  cookie = request.headers.get('Cookie')
+  records = KusisAPI(cookie).getGPA()
+  json_string = json.dumps(records)
+  r = make_response( json_string )
+  r.mimetype = 'application/json'
+  # print(json_string)
   return r
   
 web.run(app)

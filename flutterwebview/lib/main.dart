18c01beb1e0558b74cd6fa70314f83a7d66fb56b
Mete-uz <uzmete@gmail.com>
2022-04-15 23:40:38 +0300

    andriod debug + webview on web app

--- flutterwebview/lib/main.dart
+++ flutterwebview/lib/main.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutterwebview/views/home_page.dart';
+import 'dart:ui' as ui;
+import 'dart:html';
+
 
 void main() {
+  // ignore: undefined_prefixed_name
+  ui.platformViewRegistry.registerViewFactory(
+      'hello-world-html',
+          (int viewId) => IFrameElement()
+        ..width = '640'
+        ..height = '360'
+        ..src = 'https://ku.blackboard.com/'
+        ..style.border = 'none');
   runApp(const MyApp());
+  // runApp(Directionality(
+  //   textDirection: TextDirection.ltr,
+  //   child: SizedBox(
+  //     width: 640,
+  //     height: 360,
+  //     child: HtmlElementView(viewType: 'hello-world-html'),),));
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);

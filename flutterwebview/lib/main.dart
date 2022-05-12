bc5095fdef543f9cde01a4a376e593b8bc62810b
Mete-uz <uzmete@gmail.com>
2022-05-12 19:31:52 +0300

    Dialogflow integraion fix

--- flutterwebview/lib/main.dart
+++ flutterwebview/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterwebview/views/chat_main.dart';
 import 'package:flutterwebview/views/chat_page.dart';

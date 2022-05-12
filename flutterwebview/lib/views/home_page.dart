9671d837d2787b344331cac4ff91c0c9b40635e1
Alkan AKISU <aakisu19@ku.edu.tr>
2022-05-12 19:33:10 +0300

    asd

--- flutterwebview/lib/views/home_page.dart
+++ flutterwebview/lib/views/home_page.dart
@@ -2,9 +2,6 @@
 import 'dart:convert' as convert;
 import 'dart:io';
 import 'dart:math';
-
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 // import 'package:webview_cookie_manager/webview_cookie_manager.dart';
 // import 'package:webview_flutter/webview_flutter.dart';

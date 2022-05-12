e6601eb9e02b140789ca5cd316f55d176c11c5d4
burakyildirim01 <31079280+burakyildirim01@users.noreply.github.com>
2022-05-12 21:47:35 +0300

    Bridge finalized

---
+++ DialogFlow_Bridge_API.py
@@ -0,0 +1,167 @@
+import json
+import os
+import requests
+from flask import Flask
+from flask import request
+from flask import make_response
+import pymongo
+import datetime
+import time
+
+app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb+srv://kusistantt:Av8zzmtP3uiCbj3p@cluster0.bkabe.mongodb.net")
+mydb = myclient["userDB"]
+mycol = mydb["users"]
+
+userID = ''
+
+bbCookie = ""
+KUSISCookie = ""
+
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    userID = int(req.get("session").split("/")[4])
+    res = processRequest(req, userID)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def processRequest(req, userID):
+    userInfo = mycol.find_one({'id':userID})
+    bbCookie = str(userInfo['bbCookie'])
+    KUSISCookie = str(userInfo['kusisCookie'])
+    if req.get("queryResult").get("action") == "coursegrade":
+        result = req.get("queryResult")
+        parameters = result.get("parameters")
+        courseCode = parameters['course_code'].upper()
+        URL = 'https://comp491.alkanakisu.repl.co/getCourseID'
+        PARAMS = {'courseName':courseCode}
+        HEADERS = {'Cookie':bbCookie}
+        r = requests.get(url = URL, params = PARAMS, headers=HEADERS)
+        data = r.json()
+        res = getCourseGradeResult(data['courseId'], bbCookie)
+        return res
+    elif req.get("queryResult").get("action") == "lettergrade":
+        result = req.get("queryResult")
+        parameters = result.get("parameters")
+        courseCode = parameters['course_code'].upper()
+        URL = 'https://comp491.alkanakisu.repl.co/letterGrades'
+        HEADERS = {'Cookie':KUSISCookie}
+        r = requests.get(url = URL, headers=HEADERS)
+        data = r.json()
+        res = getLetterGrade(data, courseCode)
+        return res
+    elif req.get("queryResult").get("action") == "calendarevents":
+        URL = 'https://comp491.alkanakisu.repl.co/calendarEvents'
+        HEADERS = {'Cookie':bbCookie}
+        currTime, endTime = setBetweenDates()
+        PARAMS = {'from':currTime, 'to':endTime}
+        r = requests.get(url = URL, params=PARAMS, headers=HEADERS)
+        data = r.json()
+        res = getCalendarEvents(data)
+        return res
+    elif req.get("queryResult").get("action") == "getgpa":
+        URL = 'https://comp491.alkanakisu.repl.co/gpa'
+        HEADERS = {'Cookie':KUSISCookie}
+        r = requests.get(url = URL, headers=HEADERS)
+        data = r.json()
+        res = getGPA(data)
+        return res
+    elif req.get("queryResult").get("action") == "campus_weather":
+        r = requests.get("https://api.open-meteo.com/v1/forecast?latitude=41.205&longitude=29.072&daily=temperature_2m_max,temperature_2m_min&timezone=Europe%2FMoscow")
+        data = r.json()
+        res = getWeatherInfo(data)
+        return res
+
+    print("Please check the action in DialogFlow")
+    return {}
+
+def getWeatherInfo(data):
+    maxTemp = data['daily']['temperature_2m_max'][0]
+    minTemp = data['daily']['temperature_2m_min'][0]
+    weatherData = f"Max Temp: {maxTemp} / Min Temp: {minTemp}"
+    return {"fulfillmentText": weatherData}
+
+def getGPA(data):
+    GPA = data['gpa']
+    print("Your gpa is: ", GPA)
+    return {"fulfillmentText": f"Your GPA is {GPA}"}
+
+def setBetweenDates():
+    currTime = datetime.datetime.now()
+    endTime = currTime + datetime.timedelta(days=14)
+    return int(currTime.timestamp())*1000, int(endTime.timestamp())*1000
+
+def getCalendarEvents(data):
+    results = []
+    for event in data:
+        dateAndTime = event['endDate'].split('T')[0] + " / " +(event['endDate'].split('T')[1]).split('.')[0]
+        results.append(f"Course: {event['calendarId'].split('-')[0]}\nAssignment Name: {event['title']}\nDue Date: {dateAndTime}\n\n")
+    result = "-> "+"-> ".join(results)
+    return {
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+            result
+        ]
+      }
+    }
+  ]
+}
+
+
+
+def getLetterGrade(data, courseCode):
+    letterGrade = " "
+    for course in data:
+        if course['code'].replace(" ", "") == courseCode:
+            letterGrade = course['grade']
+    return {
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+            f"Your letter grade for {courseCode} is {letterGrade}"
+          ]
+      }
+    }
+  ]
+}
+
+def getCourseGradeResult(courseId, bbCookie):
+    URL = 'https://comp491.alkanakisu.repl.co/mygrades/'+str(courseId)
+    HEADERS = {'Cookie':bbCookie}
+    r = requests.get(url = URL, headers=HEADERS)
+    data = r.json()
+    gradeList = []
+    for item in data:
+        gradeList.append(f"{item['name']}: {item['grade']}/{item['maxGrade']}\n\n")
+    result = "-> "+"-> ".join(gradeList)
+    print(result)
+    return {
+  "fulfillmentMessages": [
+    {
+      "text": {
+        "text": [
+        result
+        ]
+      }
+    }
+  ]
+}
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Hello"
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5001))
+    print("Starting app on port %d" % port)
+    app.run(debug=True, port=port, host='0.0.0.0')
